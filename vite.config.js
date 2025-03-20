214aa87c84609a58e49887b85ad09a43e59d48e8
amax2019189 <amax-2019189@kinal.edu.gt>
2025-03-20 13:47:01 -0600

    cargando cambio

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Permite que el servidor devuelva `index.html` en rutas desconocidas
+  },
 })
