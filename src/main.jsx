0bb0b8a53c2a0dd96a764c6c2e859898a03dd9c8
amax2019189 <amax-2019189@kinal.edu.gt>
2025-03-20 13:40:59 -0600

    cargando cambio

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'dash.css'
+import './dash.css';
 import { App } from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
